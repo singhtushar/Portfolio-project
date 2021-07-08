a505b6558338b3f68fc09544f1c178baab8cfd85
Tushar Singh <singhtusharts22@gmail.com>
2021-07-08 19:51:53 +0530

    resume link fixed

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,24 @@
-const getCv = document.getElementById('btn');
+const getCv = document.getElementById("btn");
 
-getCv.addEventListener('click', (e) => {
+getCv.addEventListener("click", (e) => {
   window.open(
-    "https://drive.google.com/file/d/14S93u3eWpB3SIMn2vOzasyVwTTdkupd4/view?usp=sharing", '_blank'
+    "https://drive.google.com/file/d/1jVZ8pevRabq-S6wfbuzyLqc76q1DReG-/view?usp=sharing",
+    "_blank"
   );
-})
+});
 
-const css = document.getElementById('theme-style');
-const dark = document.getElementById('blue-mode');
+const css = document.getElementById("theme-style");
+const dark = document.getElementById("blue-mode");
 
-if (localStorage.getItem('theme') === 'dark') {
+if (localStorage.getItem("theme") === "dark") {
   darkTheme();
-}
-else {
+} else {
   lightTheme();
 }
 
 function darkTheme() {
   css.setAttribute("href", "css/dark.css");
-  localStorage.setItem('theme', 'dark');
+  localStorage.setItem("theme", "dark");
 }
 
 function lightTheme() {
@@ -26,12 +26,11 @@ function lightTheme() {
   localStorage.setItem("theme", "light");
 }
 
-dark.addEventListener('click', (e) => {
+dark.addEventListener("click", (e) => {
   darkTheme();
-})
+});
 
-const light = document.getElementById('light-mode')
-light.addEventListener('click', (e) => {
+const light = document.getElementById("light-mode");
+light.addEventListener("click", (e) => {
   lightTheme();
-})
-
+});
